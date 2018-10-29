69376487ab6e465381b5db0fa578594c490269d0
Felix <2680383605@qq.com>
2018-10-29 18:35:22 +0800

    🛠 fix bug in 0.36beta ver

--- packages/web3-providers-http/src/index.js
+++ packages/web3-providers-http/src/index.js
@@ -23,7 +23,7 @@
  */
 
 var errors = require('web3-core-helpers').errors;
-var XHR2 = require('xhr2-cookies').XMLHttpRequest // jshint ignore: line
+var XHR2 = require('xhr2') // jshint ignore: line
 var http = require('http');
 var https = require('https');
 
@@ -46,15 +46,15 @@ var HttpProvider = function HttpProvider(host, options) {
 
 HttpProvider.prototype._prepareRequest = function(){
     var request = new XHR2();
-    request.nodejsSet({
-        httpsAgent:this.httpsAgent,
-        httpAgent:this.httpAgent
-    });
+    // request.nodejsSet({
+    //     httpsAgent:this.httpsAgent,
+    //     httpAgent:this.httpAgent
+    // });
 
     request.open('POST', this.host, true);
     request.setRequestHeader('Content-Type','application/json');
     request.timeout = this.timeout && this.timeout !== 1 ? this.timeout : 0;
-    request.withCredentials = true;
+    // request.withCredentials = true;
 
     if(this.headers) {
         this.headers.forEach(function(header) {
