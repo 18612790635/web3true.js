f837b6a4c7c1ac3e18a8bc7caa52e61cff4579c6
tari404 <2680383605@qq.com>
2019-03-27 17:59:27 +0800

    ⚙️ update package config

---
+++ package.js
@@ -0,0 +1,23 @@
+/* jshint ignore:start */
+Package.describe({
+  name: 'truechain:web3t',
+  version: '1.0.4',
+  summary: 'Truechain JavaScript API wrapper repository',
+  git: 'https://github.com/truechain/web3true.js',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+// Npm.depends({
+//     "xmlhttprequest": "1.7.0"
+// });
+
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.2');
+
+  api.addFiles('dist/web3t.js', ['client']); // 'server'
+});
+
+/* jshint ignore:end */
