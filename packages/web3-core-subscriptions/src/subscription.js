df64d7c1b1c71b78e3db345baf53277f6048d699
tari404 <2680383605@qq.com>
2019-03-29 11:07:26 +0800

    🛠 fix bug

--- packages/web3-core-subscriptions/src/subscription.js
+++ packages/web3-core-subscriptions/src/subscription.js
@@ -221,7 +221,7 @@ Subscription.prototype.subscribe = function() {
     if(payload.params[0] === 'logs' && _.isObject(payload.params[1]) && payload.params[1].hasOwnProperty('fromBlock') && isFinite(payload.params[1].fromBlock)) {
         // send the subscription request
         this.options.requestManager.send({
-            method: 'etrue_getLogs',
+            method: this.options.requestManager.provider.genCall('getLogs'),
             params: [payload.params[1]]
         }, function (err, logs) {
             if(!err) {
