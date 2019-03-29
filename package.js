5f5bc26530dc4d382691785827594e8f89c1fcc9
tari404 <2680383605@qq.com>
2019-03-29 11:09:54 +0800

    📤 1.0.8

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 Package.describe({
   name: 'truechain:web3t',
-  version: '1.0.6',
+  version: '1.0.8',
   summary: 'Truechain JavaScript API wrapper repository',
   git: 'https://github.com/truechain/web3true.js',
   // By default, Meteor will default to using README.md for documentation.
